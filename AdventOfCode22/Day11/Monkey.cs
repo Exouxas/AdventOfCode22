bc392515b574c2c0d739610bb67a080a0af5595d
Exouxas <exouxas@gmail.com>
2022-12-23 20:17:12 +0100

    Begin day 11

---
+++ AdventOfCode22/Day11/Monkey.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode22.Day11
+{
+    internal class Monkey
+    {
+        public int[] Items { get; set; }
+        public string Operation { get; set; }
+        public int Divisible { get; set; }
+        public int TrueThrow { get; set; }
+        public int FalseThrow { get; set; }
+
+        public Monkey(int[] Items, string Operation, int Divisible, int TrueThrow, int FalseThrow) 
+        {
+            this.Items = Items;
+            this.Operation = Operation;
+            this.Divisible = Divisible;
+            this.TrueThrow = TrueThrow;
+            this.FalseThrow = FalseThrow;
+        }
+
+        public int Op(int worry)
+        {
+            string[] parts = Operation.Split(" ");
+
+            int p1;
+            int p2;
+
+            if (parts[0].Equals("old"))
+            {
+                p1 = worry;
+            }
+            else
+            {
+                p1 = int.Parse(parts[0]);
+            }
+
+            if (parts[2].Equals("old"))
+            {
+                p2 = worry;
+            }
+            else
+            {
+                p2 = int.Parse(parts[2]);
+            }
+
+
+            switch (parts[1])
+            {
+                case "+":
+                    return p1 + p2;
+                case "-":
+                    return p1 - p2;
+                case "*":
+                    return p1 * p2;
+                case "/":
+                    return p1 / p2;
+                default:
+                    throw(new Exception("Fuq"));
+            }
+        }
+    }
+}
---
+++ AdventOfCode22/Day11/Solver.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+
+namespace AdventOfCode22.Day11
+{
+    public class Solver : AdventSolver
+    {
+        public override string GetPuzzleOutput1()
+        {
+            List<Monkey> monkeys = GenerateMonkeys();
+
+
+
+            return "";
+        }
+
+        public override string GetPuzzleOutput2()
+        {
+
+
+
+            return "";
+        }
+
+        private List<Monkey> GenerateMonkeys()
+        {
+            List<Monkey> monkeys = new();
+
+            int i = 0;
+            while(i < puzzleInput.Length)
+            {
+                string[] preItems = puzzleInput[i + 1].Split("items: ")[1].Split(", ");
+                int[] items = new int[preItems.Length];
+                for(int j = 0; j < preItems.Length; j++)
+                {
+                    items[j] = int.Parse(preItems[j]);
+                }
+
+                Monkey monkey = new(
+                    items,
+                    puzzleInput[i + 2].Split("new = ")[1],
+                    int.Parse(puzzleInput[i + 3].Split("divisible by ")[1]),
+                    int.Parse(puzzleInput[i + 4].Split("to monkey ")[1]),
+                    int.Parse(puzzleInput[i + 5].Split("to monkey ")[1])
+                    );
+
+                monkeys.Add(monkey);
+                i += 7;
+            }
+
+            return monkeys;
+        }
+
+        protected override string GetFolderName()
+        {
+            return "Day11";
+        }
+    }
+}
